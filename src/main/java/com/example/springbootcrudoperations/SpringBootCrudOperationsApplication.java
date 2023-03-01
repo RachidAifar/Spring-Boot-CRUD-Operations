b67b18539668458db08c3ad5420552283e37a5a3
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2023-03-01 20:31:38 +0100

    feat<main>:make first git request

---
+++ src/main/java/com/example/springbootcrudoperations/SpringBootCrudOperationsApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootcrudoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootCrudOperationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudOperationsApplication.class, args);
+    }
+}
